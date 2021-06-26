850a70d1b55c0557dc574684def41535694a7f59
PralinJoardar <pralin.joardar@mail.vinove.com>
2021-06-26 16:50:31 +0530

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,32 @@
+import Head from "next/head";
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"></meta>
+      </Head>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          loremNostrud anim ad tempor do duis magna do dolor. Aliquip minim
+          velit id nisi consectetur reprehenderit enim. Est mollit quis do ut do
+          ad aute est sit. Ullamco incididunt culpa exercitation aute ipsum
+          excepteur cillum veniam sint quis nostrud deserunt laboris. Eu
+          adipisicing ea officia officia aliquip. Reprehenderit ut qui cillum
+          sit. Aute ad ut commodo ut dolor enim.
+        </p>
+        <p>
+          loremNostrud anim ad tempor do duis magna do dolor. Aliquip minim
+          velit id nisi consectetur reprehenderit enim. Est mollit quis do ut do
+          ad aute est sit. Ullamco incididunt culpa exercitation aute ipsum
+          excepteur cillum veniam sint quis nostrud deserunt laboris. Eu
+          adipisicing ea officia officia aliquip. Reprehenderit ut qui cillum
+          sit. Aute ad ut commodo ut dolor enim.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
